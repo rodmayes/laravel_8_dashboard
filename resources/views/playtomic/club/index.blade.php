2baf59c0f8dcae338b8823865f5b79f5629bc74d
Ester Rodriguez <rodmayes@gmail.com>
2023-09-29 00:34:43 +0200

    Changind theme Cleopatra

--- resources/views/playtomic/club/index.blade.php
+++ resources/views/playtomic/club/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.admin')
 @section('content')
-            @livewire('playtomic.club.index')
+    <h1 class="h5 mb-2">Clubs List</h1>
+    @livewire('playtomic.club.index')
 @endsection
--- resources/views/playtomic/resource/index.blade.php
+++ resources/views/playtomic/resource/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.admin')
 @section('content')
+    <h1 class="h5 mb-2">Resources List</h1>
     @livewire('playtomic.resource.index')
 @endsection
