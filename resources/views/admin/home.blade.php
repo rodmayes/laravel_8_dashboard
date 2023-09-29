3c3005924a327fd06b8b7b48f33e1fd80a5c4e3c
Ester Rodriguez <rodmayes@gmail.com>
2023-09-29 09:24:41 +0200

    Changind theme Cleopatra

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.admin')
 @section('content')
 
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Dashboard v3</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard v3</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="container-fluid">
     <div class="card-header">
         <div class="card-row">
@@ -33,6 +17,7 @@
             </div>
         @endif
 
+        <h1>Hi {{ auth()->user()->name }}!!</h1>
         <p class="pt-3">You are logged in!</p>
     </div>
 </div>
