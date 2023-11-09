8d8e1d461eef3748e6a05a8c5d19343f0e7266d4
Ester Rodriguez <rodmayes@gmail.com>
2023-11-09 22:51:36 +0100

    Bookings: Fix create from calendar selection

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -40,7 +40,7 @@
                 <a class="block px-4 py-2 text-sm hover:bg-primary-100 hover:text-gray" href="{{ route('playtomic.bookings.create', $loopdate->format('Y-m-d')) }}">
                 @php
                     $day_key = array_search($loopdate->format('Y-m-d'),array_map(function($item){ return explode(" ",$item)[0];},array_column($bookings,'started_at')), true);
-                    echo (int)$day_key."-";
+                    echo (int)$day_key;
                 @endphp
                 @if(!epmty($day_key) && (int)$day_key >= 0)
                     <span class="text-yellow-600 font-bold" data-popover-target="popover-{{$loopdate}}">
