f5d57f1581ac08fba1e5360b11454b0e0f2b1e96
Ester Rodríguez <erodriguez@doolehealth.com>
2024-01-26 14:13:04 +0100

    Fix things

--- resources/views/livewire/playtomic/booking/index.blade.php
+++ resources/views/livewire/playtomic/booking/index.blade.php
@@ -125,7 +125,7 @@
                         <input type="checkbox" value="{{ $booking->id }}" wire:model="selected" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                     </div>
                 </td>
-                <td class="px-4 py-4">
+                <td class="px-4 py-4 w-8 h-8 overflow-hidden rounded-full">
                     <img src="{{ asset($booking->creator->getAvatar()) }}" class="w-full h-full object-cover" wire:model="image">
                 </td>
                 <td class="px-2 py-2">{{ $booking->id }}</td>
